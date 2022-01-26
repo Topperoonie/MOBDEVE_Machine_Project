e9ccf8866cb46bc7949ddf4ff99971ca217a39a6
Topperoonie <75753162+Topperoonie@users.noreply.github.com>
2022-01-26 23:04:45 +0800

    Added UI to poker main and double or nothing

---
+++ app/src/main/java/com/mobdeves/s11/arjonillo/johnchristopher/mobdevemp/DoubleNothingActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeves.s11.arjonillo.johnchristopher.mobdevemp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoubleNothingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_double_nothing)
+    }
+}
